3bf7e4489fa50674700e6f58d5105d2e5452714f
Nobel Reo Jacob K S <53659103+nobelnrj@users.noreply.github.com>
2020-04-20 16:13:40 +0530

    home screen update-1

--- screens/home.js
+++ screens/home.js
@@ -1,13 +1,58 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 
 const home = () => {
-  return (
-    <Card style={styles.card}>
-      <Text>This is from home screen</Text>
-    </Card>
-  );
+  const data = [
+    {
+      id: 1,
+      photo:
+        "https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
+      name: "Nobel",
+      position: "Web developer",
+    },
+    {
+      id: 2,
+      photo:
+        "https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
+      name: "Shibu",
+      position: "Senior Web developer",
+    },
+    {
+      id: 3,
+      photo:
+        "https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
+      name: "Randip",
+      position: "Full stack developer",
+    },
+    {
+      id: 4,
+      photo:
+        "https://images.unsplash.com/flagged/photo-1578848151039-b8916d7c1c34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=821&q=80",
+      name: "Naveen",
+      position: "Backend developer",
+    },
+  ];
+
+  const renderDetails = data.map((item) => {
+    return (
+      <Card style={styles.card} key={item.id}>
+        <View style={styles.cardView}>
+          <Image
+            style={styles.img}
+            source={{
+              uri: item.photo,
+            }}
+          />
+          <View>
+            <Text>{item.name}</Text>
+            <Text>{item.position}</Text>
+          </View>
+        </View>
+      </Card>
+    );
+  });
+  return <View>{renderDetails}</View>;
 };
 
 const styles = StyleSheet.create({
@@ -15,7 +60,19 @@ const styles = StyleSheet.create({
     margin: 5,
     padding: 5,
     backgroundColor: "#6285ff",
-    width: 200,
+  },
+  cardView: {
+    flexDirection: "row",
+  },
+  img: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  text: {
+    fontSize: 20,
+    textTransform: "capitalize",
+    marginBottom: 10,
   },
 });
 
